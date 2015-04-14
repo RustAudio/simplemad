ee7f6b90ae15a97a199ca9a34d680b1c4e0d706b
Benjamin Dykstra <bendykst@gmail.com>
2015-04-14 17:08:30 +0000

    Progress on callbacks

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,38 @@ use std::default::Default;
 use std::io::BufReader;
 use std::path::Path;
 use std::fs::File;
+use std::ptr;
 use self::mad_decoder_mode::* ;
 
 #[link(name = "mad")]
 extern {
     fn mad_decoder_init(decoder: &mad_decoder,
                         message: &mad_message,
-                        input_callback: extern fn(),
+                        input_callback: extern fn(message: &mad_message, stream: isize),
                         header_callback: extern fn(),
                         filter_callback: extern fn(),
-                        output_callback: extern fn(),
+                        output_callback: extern fn(message: &mad_message,
+                                                   header: isize,
+                                                   pcm: &mad_pcm),
                         error_callback: extern fn(),
                         message_callback: extern fn());
-    fn mad_decoder_run(input: u8);
+    fn mad_decoder_run(input: &mut mad_decoder, mode: mad_decoder_mode) -> i32;
+    fn mad_stream_buffer(stream: isize, buf_start: &[u8], buf_length: usize);
 }
 
-#[derive(Default)]
+#[repr(C)]
+struct mad_pcm {
+    sample_rate: usize,
+    channels: u16,
+    length: u16,
+    samples: [[u32; 1152]; 2],
+}
+
 #[repr(C)]
 struct mad_message<'a> {
     start: &'a [u8],
-    length: u32
+    length: u32,
+    test_string: &'static str,
 }
 
 #[repr(C)]
@@ -51,9 +63,17 @@ struct mad_decoder {
     mode: mad_decoder_mode,
     options: isize,
     async: mad_async_parameters,
+    sync: usize,
+    cb_data: usize,
+    input_func: usize,
+    header_func: usize,
+    filter_func: usize,
+    output_func: usize,
+    error_func: usize,
+    message_func: usize,
 }
 
-extern fn empty_callback_fn () {
+extern fn empty_callback() {
 
 }
 
@@ -68,18 +88,32 @@ fn test_open_file() {
     let message = &mad_message {
         start: input_buffer,
         length: input_buffer.len() as u32,
+        test_string: "This is a test.",
     };
 
     let mut decoder: mad_decoder = Default::default();
+    let mut decoding_result: i32 = 42;
+
+    extern fn input_callback (message: &mad_message, stream: isize) {
+        panic!("{}", message.test_string);
+        // mad_stream_buffer(stream, message.start, message.length as usize);
+    }
+
+    extern fn output_callback(message: &mad_message, header: isize, pcm: &mad_pcm) {
+        panic!("In OUTPUT!");
+    }
 
     unsafe {
         mad_decoder_init(&mut decoder,
                          message,
-                         empty_callback_fn,
-                         empty_callback_fn,
-                         empty_callback_fn,
-                         empty_callback_fn,
-                         empty_callback_fn,
-                         empty_callback_fn);
+                         input_callback,
+                         empty_callback,
+                         empty_callback,
+                         output_callback,
+                         empty_callback,
+                         empty_callback);
+        decoding_result = mad_decoder_run(&mut decoder, mad_decoder_mode::MAD_DECODER_MODE_SYNC);
     }
+
+    assert_eq!(decoding_result, 0);
 }
