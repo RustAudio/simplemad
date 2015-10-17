c7d6393946bf0536c236b4f34a0e96c77bc7948e
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-10-17 10:52:11 +0200

    Add architecture specific optimizations

--- simplemad_sys/build.rs
+++ simplemad_sys/build.rs
@@ -9,36 +9,51 @@ fn main() {
         Err(..) => {}
     };
 
+    // attempt to compile libmad manually
+    let mut gcc = gcc::Config::new();
+    gcc.include("libmad-src");
+
     let target = env::var("TARGET").unwrap();
-    let fpm = if target.contains("i686") {
-        "FPM_INTEL"
+
+    if target.contains("i686") {
+        gcc.define("FPM_INTEL", None);
+        gcc.define("ASO_ZEROCHECK", None);
+
     } else if target.contains("x86_64") {
-        "FPM_64BIT"
+        gcc.define("FPM_64BIT", None);
+
     } else if target.contains("arm") {
-        "FPM_ARM"
+        gcc.define("FPM_ARM", None);
+        // TODO: I'm not sure if compiling assembly works
+        //gcc.define("ASO_INTERLEAVE1", None);
+        //gcc.define("ASO_IMDCT", None);
+        //gcc.file("libmad-src/imdct_l_arm.S");
+
     } else if target.contains("mips") {
-        "FPM_MIPS"
+        gcc.define("FPM_MIPS", None);
+        gcc.define("ASO_INTERLEAVE2", None);
+        gcc.define("ASO_ZEROCHECK", None);
+
     } else if target.contains("sparc") {
-        "FPM_SPARC"
+        gcc.define("FPM_SPARC", None);
+
     } else if target.contains("ppc") || target.contains("powerpc") {
-        "FPM_PPC"
+        gcc.define("FPM_PPC", None);
+
     } else {
-        "FPM_DEFAULT"
+        gcc.define("FPM_DEFAULT", None);
     };
 
-    gcc::Config::new()
-                .include("libmad-src")
-                .define(&fpm, None)
-                .file("libmad-src/version.c")
-                .file("libmad-src/fixed.c")
-                .file("libmad-src/bit.c")
-                .file("libmad-src/timer.c")
-                .file("libmad-src/stream.c")
-                .file("libmad-src/frame.c")
-                .file("libmad-src/synth.c")
-                .file("libmad-src/decoder.c")
-                .file("libmad-src/layer12.c")
-                .file("libmad-src/layer3.c")
-                .file("libmad-src/huffman.c")
-                .compile("libmad.a");  // the official name is `libmad.la` but gcc-rs forbids that
+    gcc.file("libmad-src/version.c")
+       .file("libmad-src/fixed.c")
+       .file("libmad-src/bit.c")
+       .file("libmad-src/timer.c")
+       .file("libmad-src/stream.c")
+       .file("libmad-src/frame.c")
+       .file("libmad-src/synth.c")
+       .file("libmad-src/decoder.c")
+       .file("libmad-src/layer12.c")
+       .file("libmad-src/layer3.c")
+       .file("libmad-src/huffman.c")
+       .compile("libmad.a");  // the official name is `libmad.la` but gcc-rs forbids that
 }
