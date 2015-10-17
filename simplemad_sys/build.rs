63211c50e42145c2ff5c95b6c5b9ede2aca5e3f5
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-10-17 10:39:43 +0200

    Compile libmad properly

--- simplemad_sys/build.rs
+++ simplemad_sys/build.rs
@@ -1,74 +1,44 @@
 extern crate gcc;
 extern crate pkg_config;
 
-use std::process::Command;
-use std::path::Path;
 use std::env;
 
 fn main() {
-    let out_dir_str = env::var("OUT_DIR").unwrap();
-    let out_dir = Path::new(&out_dir_str);
-
     match pkg_config::find_library("mad") {
         Ok(_) => return,
         Err(..) => {}
     };
 
-    if cfg!(windows) {
-        panic!("libmad could not be found. Building libmad automatically on Windows not implemented yet. Please install libmad manually.");
-    }
+    let target = env::var("TARGET").unwrap();
+    let fpm = if target.contains("i686") {
+        "FPM_INTEL"
+    } else if target.contains("x86_64") {
+        "FPM_64BIT"
+    } else if target.contains("arm") {
+        "FPM_ARM"
+    } else if target.contains("mips") {
+        "FPM_MIPS"
+    } else if target.contains("sparc") {
+        "FPM_SPARC"
+    } else if target.contains("ppc") || target.contains("powerpc") {
+        "FPM_PPC"
+    } else {
+        "FPM_DEFAULT"
+    };
 
-    match env::set_current_dir("libmad-src") {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    match Command::new("mkdir").arg("temp").output() {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    match Command::new("tar").arg("-xzf")
-                             .arg("libmad-0.15.1b.patched.tar.gz")
-                             .arg("-C")
-                             .arg("temp")
-                             .output() {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    match env::set_current_dir("temp/libmad-0.15.1b/") {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    Command::new("./configure")
-        .args(&["--disable-shared", "--enable-static"]) // Only build static lib
-        .args(&["--prefix", out_dir.to_str().unwrap()]) // Install on the outdir
-        .arg("--with-pic") // Build position-independent code (required by Rust)
-        .output()
-        .unwrap();
-
-    match Command::new("make").output() {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    match Command::new("make").arg("install").output() {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    match env::set_current_dir("../..") {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    match Command::new("rm").arg("-rf").arg("temp").output() {
-        Ok(_) => {},
-        Err(e) => panic!("{}", e)
-    }
-
-    let out_str = out_dir.to_str().unwrap();
-    println!("cargo:rustc-flags=-L native={}/lib -l static=mad", out_str);
+    gcc::Config::new()
+                .include("libmad-src")
+                .define(&fpm, None)
+                .file("libmad-src/version.c")
+                .file("libmad-src/fixed.c")
+                .file("libmad-src/bit.c")
+                .file("libmad-src/timer.c")
+                .file("libmad-src/stream.c")
+                .file("libmad-src/frame.c")
+                .file("libmad-src/synth.c")
+                .file("libmad-src/decoder.c")
+                .file("libmad-src/layer12.c")
+                .file("libmad-src/layer3.c")
+                .file("libmad-src/huffman.c")
+                .compile("libmad.a");  // the official name is `libmad.la` but gcc-rs forbids that
 }
